86c5f9d3f85439fb4e5a372cf7bfbbbc3161aff9
siddiq <siddiq9950@gmail.com>
2022-10-21 02:34:19 +0530

    added user login feature

--- app/src/main/java/com/example/android/myinstagram/LoginActivity.kt
+++ app/src/main/java/com/example/android/myinstagram/LoginActivity.kt
@@ -1,11 +1,57 @@
 package com.example.android.myinstagram
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var email:EditText
+    lateinit var password:EditText
+    lateinit var register:TextView
+    lateinit var login:Button
+    lateinit var auth:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        email=findViewById(R.id.email1)
+        password=findViewById(R.id.password1);
+        register=findViewById(R.id.register_user)
+        login=findViewById(R.id.login1)
+        auth= FirebaseAuth.getInstance()
+        register.setOnClickListener {
+            startActivity(Intent(this,RegisterActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+        }
+        login.setOnClickListener {
+            var eml_txt:String=email.text.toString()
+            var psw_txt:String=password.text.toString()
+            if(TextUtils.isEmpty(eml_txt) || TextUtils.isEmpty(psw_txt)){
+                Toast.makeText(this,"Empty Credentials!",Toast.LENGTH_LONG).show()
+            }
+            else{
+                loginUser(eml_txt,psw_txt);
+            }
+        }
+    }
+
+    private fun loginUser(emlTxt: String, pswTxt: String) {
+        auth.signInWithEmailAndPassword(emlTxt,pswTxt).addOnCompleteListener {
+            if(it.isSuccessful){
+                Toast.makeText(
+                    this,
+                    "update the profile for better Experience!",
+                    Toast.LENGTH_LONG
+                ).show()
+            startActivity(Intent(this,MainActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+            finish()
+            }
+        }.addOnFailureListener {
+            Toast.makeText(this,"check your email and passsword",Toast.LENGTH_LONG).show()
+        }
     }
 }
--- app/src/main/java/com/example/android/myinstagram/RegisterActivity.kt
+++ app/src/main/java/com/example/android/myinstagram/RegisterActivity.kt
@@ -1,18 +1,14 @@
 package com.example.android.myinstagram
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
-import android.view.TextureView
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import org.w3c.dom.Text
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var username:EditText
@@ -21,7 +17,7 @@ class RegisterActivity : AppCompatActivity() {
     private lateinit var password:EditText
     private lateinit var register:Button
     private lateinit var login:TextView
-
+    private lateinit var pd:ProgressDialog
     private lateinit var auth:FirebaseAuth
     private lateinit var mrootRef: DatabaseReference
 
@@ -35,7 +31,7 @@ class RegisterActivity : AppCompatActivity() {
         password=findViewById(R.id.password)
         login=findViewById(R.id.login_user)
         register=findViewById(R.id.register1)
-
+        pd= ProgressDialog(this)
         login.setOnClickListener {
             startActivity(Intent(this,LoginActivity::class.java))
         }
@@ -55,11 +51,11 @@ class RegisterActivity : AppCompatActivity() {
                 registerUser(txtusername,txtname,txtemail,txtpassword);
             }
         }
-        
-
     }
 
      fun registerUser(txtusername: String, txtname: String, txtemail: String, txtpassword: String) {
+         pd.setMessage("please wait..")
+         pd.show()
         auth.createUserWithEmailAndPassword(txtemail,txtpassword).addOnSuccessListener {
             var hm:HashMap<String,String> = HashMap<String,String>()
             hm.put("name",txtname)
@@ -67,12 +63,19 @@ class RegisterActivity : AppCompatActivity() {
             hm.put("username",txtusername)
             auth.currentUser?.let { it1 -> hm.put("id", it1.uid) }
             auth.currentUser?.let { it1 -> mrootRef.child("Users").child(it1.uid).setValue(hm).addOnCompleteListener {
-                if(it.isSuccessful)
-                    Toast.makeText(this,"update the profile for better Experience!",Toast.LENGTH_LONG).show()
+                if(it.isSuccessful) {
+                    pd.dismiss()
+                    Toast.makeText(
+                        this,
+                        "update the profile for better Experience!",
+                        Toast.LENGTH_LONG
+                    ).show()
+
                 startActivity(Intent(this,MainActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
                 finish()
-
+                }
             }.addOnFailureListener {
+                pd.dismiss()
                 Toast.makeText(this,"something went wrong",Toast.LENGTH_LONG).show()
             } }
         }
